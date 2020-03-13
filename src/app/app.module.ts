587dc0e50ca1f86b43cc5be06dbfbebdc39b8c44
Kfir <kfirprods@gmail.com>
2020-03-13 23:46:10 +0200

    Added missing svg files; Gallery timer animation works

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,19 +1,23 @@
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { NgModule } from '@angular/core';
 import { FlexLayoutModule } from '@angular/flex-layout';
 
 import { PhotoSlideComponent } from './components/photo-slide/photo-slide.component';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { CommonModule } from '@angular/common';
 
 @NgModule({
   declarations: [
-    PhotoSlideComponent,
-    AppComponent
+    AppComponent,
+    PhotoSlideComponent
   ],
   imports: [
+    CommonModule,
     FlexLayoutModule,
     BrowserModule,
+    BrowserAnimationsModule,
     AppRoutingModule
   ],
   providers: [],
--- src/app/components/photo-slide/photo-slide.component.ts
+++ src/app/components/photo-slide/photo-slide.component.ts
@@ -1,12 +1,26 @@
 import { Component, OnInit, Input } from '@angular/core';
+import { trigger, state, style, animate, transition } from '@angular/animations';
+import { ThrowStmt } from '@angular/compiler';
 
 @Component({
   selector: 'app-photo-slide',
   templateUrl: './photo-slide.component.html',
-  styleUrls: ['./photo-slide.component.scss']
+  styleUrls: ['./photo-slide.component.scss'],
+  animations: [
+    trigger('photoLoaded', [
+      state('start', style({
+        width: '0px'
+      })),
+      state('end', style({
+        width: '100%'
+      })),
+      transition('start => end', [animate('{{duration}}s')]),
+    ])
+  ]
 })
 export class PhotoSlideComponent implements OnInit {
   currentImage: string;
+  isLoadingNextImage: boolean;
   @Input() images: string[];
   @Input() interval = 6;
 
@@ -36,9 +50,16 @@ export class PhotoSlideComponent implements OnInit {
   }
 
   changeCurrentImage(image: string) {
+    this.isLoadingNextImage = true;
     this.currentImage = image;
     this.currentImageIndex = this.images.indexOf(image);
 
     this.setupTimer();
+  }
+
+  reanimate(event) {
+    if (event.toState === 'start') {
+      this.isLoadingNextImage = false;
+    }
   }
 }
