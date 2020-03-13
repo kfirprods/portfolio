7891b7d9aea365f9190af9affba3fc4e4ed58971
Kfir <kfirprods@gmail.com>
2020-03-13 20:37:06 +0200

    Very basic page with static content only

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,18 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FlexLayoutModule } from '@angular/flex-layout';
 
+import { PhotoSlideComponent } from './components/photo-slide/photo-slide.component';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
 @NgModule({
   declarations: [
+    PhotoSlideComponent,
     AppComponent
   ],
   imports: [
+    FlexLayoutModule,
     BrowserModule,
     AppRoutingModule
   ],
---
+++ src/app/components/photo-slide/photo-slide.component.ts
@@ -0,0 +1,44 @@
+import { Component, OnInit, Input } from '@angular/core';
+
+@Component({
+  selector: 'app-photo-slide',
+  templateUrl: './photo-slide.component.html',
+  styleUrls: ['./photo-slide.component.scss']
+})
+export class PhotoSlideComponent implements OnInit {
+  currentImage: string;
+  @Input() images: string[];
+  @Input() interval = 6;
+
+  private currentImageIndex = 0;
+  private timer: any;
+
+  constructor() {
+  }
+
+  ngOnInit(): void {
+    if (this.images && this.images.length > 0) {
+      this.changeCurrentImage(this.images[0]);
+    }
+
+    this.setupTimer();
+  }
+
+  private setupTimer() {
+    if (this.timer) {
+      clearInterval(this.timer);
+    }
+
+    this.timer = setInterval(() => {
+      this.currentImageIndex = (this.currentImageIndex + 1) % this.images.length;
+      this.changeCurrentImage(this.images[this.currentImageIndex]);
+    }, this.interval * 1000);
+  }
+
+  changeCurrentImage(image: string) {
+    this.currentImage = image;
+    this.currentImageIndex = this.images.indexOf(image);
+
+    this.setupTimer();
+  }
+}
